718a79c5335242a8f96a363c12f6c775f391863a
/ N|®M@L ヤ 『𝐔𝐋𝐓𝐑𝐀 𝐕𝐏𝐍』🔥 <141761993+TecnoDeVolper2008@users.noreply.github.com>
2024-10-06 20:28:08 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const {cmd , commands} = require('../command')
+const { cmd, commands } = require('../command');
 
 cmd({
     pattern: "science",
@@ -8,41 +8,34 @@ cmd({
     filename: __filename
 },
 
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-const voice = {
-    menu: 'media/AUD-20240904-WA0432.mp3'
-}
-let dec = `*👋 Hello ${pushname}*
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const voice = {
+            menu: 'media/AUD-20240904-llWA0432.mp3'
+        };
+        
+        let dec = `👋 Hello ${pushname}`;
 
-*╭───────────◉◉►*
-*│Hey, I'm Not Bot/test, Created By*
-*│* > Kawdhitha✨🍂
-*│* > ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋᴀᴡᴅʜɪᴛʜᴀ ɴɪʀᴍᴀʟッ
-*╰───────────◉◉►*
+        const pdfLinks = [
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32074/2007-2.pdf', name: 'Ol 2007' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32073/2008-3.pdf', name: 'Ol 2008' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32075/2009-3.pdf', name: 'Ol 2009' },
+            { url: 'https://downloads.pastpapers.wiki/download/38/2010-o-l/452/science-2010-sinhala-medium.pdf', name: 'Science 2010 Sinhala Medium' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32070/2011-4.pdf', name: 'Ol 2011' },
+        ];
 
-*╭───────────◎◎►*
-*│ Find Notes*
-*│ ─────────*
-*│►[.ithihase]*
-*│*
-*│*
-*│*
-*│*
-*╰───────────◎◎►*
-*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋᴀᴡᴅʜɪᴛʜᴀ ɴɪʀᴍᴀʟッ*`
-// PDF URL
-const pdfUrl = 'https://pastpapers.wiki/download/23693/20222023-o-l/72430/2020-ol-tamil-language-and-literature-past-paper-2.pdf';
+        // Send each PDF with a caption
+        for (const pdf of pdfLinks) {
+            await conn.sendMessage(from, {
+                document: { url: pdf.url },
+                mimetype: 'application/pdf',
+                fileName: pdf.name,
+                caption: dec
+            }, { quoted: mek });
+        }
 
-// Send the PDF with a caption
-await conn.sendMessage(from, {
-    document: { url: pdfUrl },  // The PDF file URL
-    mimetype: 'application/pdf', // MIME type for PDF
-    fileName: 'O-Level Tamil Language and Literature Past Paper.pdf', // Optional: Name for the PDF
-    caption: dec // The caption you want to send with the PDF
-}, { quoted: mek }); // Optionally, quote a previous message (mek)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
