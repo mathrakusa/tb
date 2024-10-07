7cc6506f36c576808d38668cceb8491adabd832d
/ N|®M@L ヤ 『𝐔𝐋𝐓𝐑𝐀 𝐕𝐏𝐍』🔥 <141761993+TecnoDeVolper2008@users.noreply.github.com>
2024-10-07 21:35:25 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,22 +6,20 @@ cmd({
     category: "science",
     react: "📜",
     filename: __filename
-},
-
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const voice = {
             menu: 'media/AUD-20240904-llWA0432.mp3'
         };
         
-        let dec = `👋 Hello ${pushname}. Powered by kawdhitha nirmal.`;
+        let dec = `👋 Hello ${pushname}. Powered by Kawdhitha Nirmal.`;
 
         const pdfLinks = [
-            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32074/2007-2.pdf', name: 'Ol 2007' },
-            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32073/2008-3.pdf', name: 'Ol 2008' },
-            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32075/2009-3.pdf', name: 'Ol 2009' },
-            { url: 'https://downloads.pastpapers.wiki/download/38/2010-o-l/452/science-2010-sinhala-medium.pdf', name: 'Science 2010 Sinhala Medium' },
-            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32070/2011-4.pdf', name: 'Ol 2011' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32074/2007-2.pdf', name: 'Ol 2007', caption: 'Ol 2007 = Kawdhitha' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32073/2008-3.pdf', name: 'Ol 2008', caption: 'Ol 2008 = Nirmal' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32075/2009-3.pdf', name: 'Ol 2009', caption: 'Ol 2009' },
+            { url: 'https://downloads.pastpapers.wiki/download/38/2010-o-l/452/science-2010-sinhala-medium.pdf', name: 'Science 2010 Sinhala Medium', caption: 'Science 2010' },
+            { url: 'https://pastpapers.wiki/download/7819/gce-o-l/32070/2011-4.pdf', name: 'Ol 2011', caption: 'Ol 2011' },
         ];
 
         // Send each PDF with the updated caption
@@ -30,7 +28,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 document: { url: pdf.url },
                 mimetype: 'application/pdf',
                 fileName: pdf.name,
-                caption: `${dec}\nDownload link: ${pdf.url}`
+                caption: `${dec}\n${pdf.caption}\nDownload link: ${pdf.url}`
             }, { quoted: mek });
         }
 
