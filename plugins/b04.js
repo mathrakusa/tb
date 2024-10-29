c75aeb4adcfb033acdb5731f3218af8946e2b1d0
/ N|®M@L ヤ 『𝐔𝐋𝐓𝐑𝐀 𝐕𝐏𝐍』🔥 <141761993+TecnoDeVolper2008@users.noreply.github.com>
2024-10-29 17:05:05 +0530

    Create b04.js

---
+++ plugins/b04.js
@@ -0,0 +1,34 @@
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "11b_4",
+    desc: "menu the bot",
+    category: "buddagama",
+    react: "✨",
+    filename: __filename
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const voice = {
+            menu: 'media/AUD-20240904-llWA0432.mp3'
+        };
+
+        const dec = `👋 Hello ${pushname}`;
+
+        const pdfLinks = [
+            { url: 'https://pastpapers.wiki/download/7816/grade-11/40862/4-2.pdf', name: 'සමාදි ගත සුතක මහිම🙏', caption: '*💪දිනලම ඉවර කරමු🔥 9A*' },
+        ];
+        // Send each PDF with the updated caption
+        for (const pdf of pdfLinks) {
+            await conn.sendMessage(from, {
+                document: { url: pdf.url },
+                mimetype: 'application/pdf',
+                fileName: pdf.name,
+                caption: `${dec}\n${pdf.caption}\n\n> Powered by Kawdhitha Nirmal`
+            }, { quoted: mek });
+        }
+
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
